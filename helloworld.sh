5e13a21de07e865d33f838d28aec4c7803c44c36
Shirihan <shrihan.p1@gmail.com>
2021-06-25 21:03:43 -0600

    Inspect groups and sudo user and scalate perms

--- helloworld.sh
+++ helloworld.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "-------Getting basic information-----------"
 echo "$USER"
 basshrcfile="/home/$USER/.bashrc"
 if test -f "$basshrcfile"; then
@@ -7,4 +8,17 @@ if test -f "$basshrcfile"; then
     grep -E '^\w+=' "$basshrcfile" 
 else
     echo "Profile file doesn't exits"
+fi
+
+echo "-------Getting Information about groups------"
+
+GROUPS_USER="$(id -nG)"
+for i in $GROUPS_USER; do
+    echo "$i"
+done
+if echo "$GROUPS_USER" | grep 'sudo'; then
+    echo "$USER is a sudo member and you can run commands as root"
+    echo "Scalating privileges"
+    ROOT=$(sudo whoami)
+    echo "Now I'm the $ROOT user"
 fi
