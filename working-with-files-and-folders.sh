af58d81c7adb26132609821dd5f87522cc63437a
Shirihan-P <shrihan.p1@gmail.com>
2021-06-26 14:13:00 -0600

    Add more filters to find

--- working-with-files-and-folders.sh
+++ working-with-files-and-folders.sh
@@ -11,8 +11,16 @@ if [ "$code" -eq "0" ]; then
     home=$(echo "$data" | awk -F  ":" '/1/ {print $6}')
     echo "The user home directory is: $home"
     echo "Lets look at the files in $username home directory."
-    documents=$(sudo find "$home" | wc -l)
-    echo "The number of files is: $documents"
+    echo "Depending on the number of data that the user has in" \
+    "the $home is the time it process will take to finish"
+    dirs=$(sudo find "$home" -type d | wc -l)
+    echo "The number of directories is: $dirs"
+    files=$(sudo find "$home" -type f | wc -l)
+    echo "The number of files is: $files"
+    symlink=$(sudo find "$home" -type l | wc -l)
+    echo "The number of symbolic links is: $symlink"
+    symlink=$(sudo find "$home" -type s | wc -l)
+    echo "The number of sockets is: $symlink"
 else
     echo "Not found user"
 fi
